ac017361e6c0fd700172b0557f348c8e8d182d50
Vasil Topalovic <vasiltopalovic@outlook.com>
2025-01-10 22:52:35 -0500

    feat: add clearing screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::{execute, terminal};
+
+fn main() -> std::io::Result<()> {
+    let mut stdout = io::stdout();
+
+    execute!(stdout, terminal::EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+    execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
+
+    Ok(())
 }
