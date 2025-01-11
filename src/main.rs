04b3a3973ef6eb0dbf03fad7512ffb63351a49a2
Vasil Topalovic <vasiltopalovic@outlook.com>
2025-01-11 13:40:22 -0500

    feat: add basic character writing

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::{
 use clap::Parser;
 use crossterm::{
     cursor,
-    event::{read, Event, KeyCode, KeyEventKind},
+    event::{read, Event, KeyCode, KeyEvent, KeyEventKind},
     execute, queue,
-    style::Print,
+    style::{self, Print},
     terminal,
 };
 use ropey::Rope;
@@ -39,7 +39,7 @@ impl Editor {
         )?;
 
         for line in self.text.lines() {
-            queue!(self.stdout, Print(line))?;
+            queue!(self.stdout, style::Print(line), style::Print("\r"))?;
         }
 
         self.stdout.flush()?;
@@ -52,6 +52,44 @@ impl Editor {
         terminal::disable_raw_mode()?;
         Ok(())
     }
+
+    fn get_cursor_index(&self) -> std::io::Result<usize> {
+        let pos = cursor::position()?;
+
+        let mut count = 0;
+        for (i, line) in self.text.lines().enumerate() {
+            if i == pos.0.into() {
+                count += pos.1 as usize;
+            } else {
+                count += line.len_chars();
+            }
+        }
+
+        Ok(count)
+    }
+
+    fn handle_events(&mut self) -> std::io::Result<bool> {
+        let event = read()?;
+
+        match event {
+            Event::Key(event) if event.kind == KeyEventKind::Press => {
+                if event.code == KeyCode::Esc {
+                    return Ok(false);
+                }
+
+                if event.code == KeyCode::Enter {
+                    self.text.insert(self.get_cursor_index()?, "\n\r");
+                }
+
+                if let KeyCode::Char(c) = event.code {
+                    self.text.insert_char(self.get_cursor_index()?, c);
+                }
+            }
+            _ => {}
+        };
+
+        Ok(true)
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -63,16 +101,11 @@ fn main() -> std::io::Result<()> {
     editor.update_display()?;
 
     loop {
-        let event = read()?;
-
-        match event {
-            Event::Key(event) if event.kind == KeyEventKind::Press => {
-                if event.code == KeyCode::Esc {
-                    break;
-                }
-            }
-            _ => {}
+        if !editor.handle_events()? {
+            break;
         }
+
+        editor.update_display()?;
     }
 
     editor.close()?;
